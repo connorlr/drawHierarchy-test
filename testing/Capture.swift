ac1a45535fe01143e3fddda85cc850f3a0ae30f1
Connor Blanck <connor@logrocket.com>
2021-07-20 13:05:13 -0400

    Add `isHidden` code

--- testing/Capture.swift
+++ testing/Capture.swift
@@ -5,10 +5,13 @@ class Capture {
   static func capture() {
     if let rootView = UIApplication.shared.windows.first?.rootViewController?.view {
       let pdfRenderer = UIGraphicsPDFRenderer()
+      let childView = rootView.subviews.first!
+      childView.isHidden = true
       pdfRenderer.pdfData {context in
         context.beginPage()
         rootView.drawHierarchy(in: rootView.bounds, afterScreenUpdates: true)
       }
+      childView.isHidden = false
     }
   }
 }
